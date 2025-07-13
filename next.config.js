fe5462c13d5b17eed6f82c11a634883ee1ac504e
MayorMax <drmayor2004@yahoo.com>
2025-07-13 22:13:23 +0100

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.pexels.com', 'cgafrica-image.s3.eu-west-2.amazonaws.com'],
+    domains: ['images.pexels.com', 'cgafrica-image.s3.eu-west-2.amazonaws.com', 'img.youtube.com'],
   },
   experimental: {
     appDir: true,
